ce88d941a70291c14403cb29a0f27d0c06e83247
Alberto Arduini <alberto.arduini3@studio.unibo.it>
2023-06-26 11:13:12 +0200

    Added reset and style css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: './reset.css',
+                    crossorigin: ''
+                },
+                {
+                    rel: 'stylesheet',
+                    href: './theme.css',
+                    crossorigin: ''
+                }
+            ]
+        }
+    }
 })
