79e4ec4445de79406c1d3e5e77eab543ab1371c3
Sabuh <mastersabuh@gmail.com>
2023-07-05 19:10:37 +0200

    added basic chart components

---
+++ pages/Chart.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+
+
+Chart.register(...registerables);
+
+const dataValues = ref([3, 4, 1, 6, 8]);
+const dataLabels = ref(['5', '4', '3', '2', '1']);
+
+
+
+const testData = reactive({
+  labels: dataLabels.value,
+  datasets: [
+    {
+      data: dataValues.value,
+      backgroundColor: ['blue', 'red', 'white', 'green', 'yellow'],
+    },
+  ],
+});
+
+</script>
+
+
+
+<template>
+  <DoughnutChart :chartData = "testData"></DoughnutChart>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import Chart from "~/pages/Chart.vue";
+</script>
+
+
+
+<template>
+  <p>CIAO</p>
+  <Chart></Chart>
+</template>
+
+<style scoped>
+
+</style>
