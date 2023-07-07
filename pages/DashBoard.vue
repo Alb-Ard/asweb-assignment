adf96cb75d286ae3eb4c8216d57a114d1895a85f
Sabuh <mastersabuh@gmail.com>
2023-07-08 01:04:37 +0200

    separate ChartBoard from DashBoard component

---
+++ pages/DashBoard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import ChartBoard from "~/components/ChartBoard.vue";
+</script>
+
+<template>
+  <section>
+    <ChartBoard></ChartBoard>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
