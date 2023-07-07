66eeec8655c6f0c4dfd8a7958ec68dad6f25cb07
Sabuh <mastersabuh@gmail.com>
2023-07-07 12:02:02 +0200

    added option to fill star review by reactive data; review div added to PlaceCard

--- components/PlaceCard.vue
+++ components/PlaceCard.vue
@@ -1,43 +1,54 @@
 <script setup lang="ts">
+  const starRating = ref(3.5);
+
+  function renderStarRating(fullStar: number, halfStar: number): string {
+    return (starRating.value >= fullStar) ? 'fa fa-star' : ((starRating.value >= halfStar) ? 'fa fa-star-half-full' : 'fa fa-star-o')
+  }
+
 
 </script>
 
 <template>
-  <div>
+  <div class="card">
     <img alt="" src="https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg">
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star-half-full"></span>
-      <span class="fa fa-star-o"></span>
-      <span class="fa fa-star-o"></span>
+    <div class="review">
+        <span :class="renderStarRating(1, 0.5)"></span>
+        <span :class="renderStarRating(2, 1.5)"></span>
+        <span :class="renderStarRating(3, 2.5)"></span>
+        <span :class="renderStarRating(4, 3.5)"></span>
+        <span :class="renderStarRating(5, 4.5)"></span>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  div {
+  .card {
     width: 30%;
     padding: 2px;
     border-radius: 10px;
   }
   img {
     width: 100%;
-    border-radius: 10px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
   }
 
-  .fa, .fa-star {
-    width: 10px;
-    height: 10px;
-    display: inline-block;
-  }
 
+  .review {
+    background-color: cornflowerblue;
+    text-align: center;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
 
   span {
-    width: 20px;
+    width: 25px;
     height: 20px;
     font-size: 40px;
     display: inline-block;
-    margin: 12px;
     color: gold;
+    margin-left: 10px;
+    margin-right: 30px;
   }
 
 
